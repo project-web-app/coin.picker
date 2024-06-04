575804b29df9a930100bc5ea36bf01026bbfd9f8
project-web-app <bandiraj209@gmail.com>
2024-06-04 20:38:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+
+const numbers =[];
+for(let i=0;i<101;i++){
+    numbers.push(i);
+}
+console.log("Original list of numbers:", numbers);
+
+const poppedList = [];
+
+function getRandomElement(arr) {
+    const index = Math.floor(Math.random() * arr.length);
+    return arr[index];
+}
+
+while (numbers.length > 0) {
+
+    document.getElementById("btn-1").addEventListener("click",displayer); 
+    button.addEventListener("click", calculateBMI); 
+}
+function displayer()
+{
+    const choiceElement = getRandomElement(numbers);
+    console.log("Chosen element:", choiceElement);
+    if(choiceElement != undefined){document.querySelector("h").innerHTML=choiceElement;}
+    else{document.querySelector("h").innerHTML="Completed";}
+    const index = numbers.indexOf(choiceElement);
+    
+        numbers.splice(index, 1);
+        poppedList.push(choiceElement);
+        document.querySelector("h2").innerHTML=poppedList;
+
+}
